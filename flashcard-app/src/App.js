2cb4bd1b43a74bb63a86090d1e70f4fbae59707f
Daniel Kuang <danielkuang7@github.com>
2020-05-05 20:19:56 -0400

    Created react app and files

---
+++ flashcard-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState} from 'react';
+import FlashcardList from './FlashcardList';
+
+function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  return (
+    <FlashcardList flashcards = {flashcards} />
+  );
+}
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: [
+      '2',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+
+  {
+    id: 2,
+    question: 'What is 3 + 3?',
+    answer: '6',
+    options: [
+      '2',
+      '3',
+      '4',
+      '6'
+    ]
+  }
+]
+
+export default App;
