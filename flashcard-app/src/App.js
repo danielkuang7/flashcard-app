097278b596e2f671ddeafa8fe4df507771a25797
Daniel Kuang <danielkuang7@github.com>
2020-05-07 09:27:54 -0400

    Added styling to the flashcard

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -11,11 +11,13 @@ function App() {
       .get('https://opentdb.com/api.php?amount=10')
       .then(res => {
         setFlashcards(res.data.results.map((questionItem, index) => {
-          const answer = questionItem.correct_answer
-          const options = [...questionItem.incorrect_answers, answer]
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)),
+             answer]
           return {
             id: `${index}-${Date.now()}`,
-            question: questionItem.question,
+            question: decodeString(questionItem.question),
             answer: answer,
             options: options.sort(() => Math.random() - .5)
           }
@@ -23,8 +25,16 @@ function App() {
       })
   }, [])
 
+  function decodeString(str){
+    const textArea = document.createElement ('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
+    <div className="container">
     <FlashcardList flashcards = {flashcards} />
+    </div>
   );
 }
 
